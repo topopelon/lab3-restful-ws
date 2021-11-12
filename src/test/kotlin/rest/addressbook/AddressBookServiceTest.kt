7a56835ba939ee7c3ce7e8df26979a00ab741840
topopelon <jlaguna98@gmail.com>
2021-11-12 22:37:22 +0100

    All tests finished.

--- src/test/kotlin/rest/addressbook/AddressBookServiceTest.kt
+++ src/test/kotlin/rest/addressbook/AddressBookServiceTest.kt
@@ -1,10 +1,10 @@
 package rest.addressbook
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.*
@@ -25,6 +25,8 @@ class AddressBookServiceTest {
     @BeforeEach
     fun cleanRepository() {
         addressBook.clear()
+        Mockito.clearAllCaches()
+        //personListSpy = Mockito.spy(addressBook.personList)
     }
 
     @Test
@@ -38,7 +40,7 @@ class AddressBookServiceTest {
                 Array<Person>::class.java
             )
             assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-            assertEquals(200, response.statusCode.value())
+            assertEquals(HttpStatus.OK, response.statusCode)
             assertEquals(0, response.body?.size)
             assertEquals(0, addressBook.personList.size)
         }
@@ -53,11 +55,10 @@ class AddressBookServiceTest {
 
     @Test
     fun createUser() {
-        // Check if [personList] is empty before GET request
-        assertEquals(0, addressBook.personList.size)
         // Prepare data
         val juan = Person(name = "Juan")
-        // Every loop information is different
+
+        // Tests user creation
         val n = (1..3)
         n.forEach { i ->
             // POST [juan] [n] times
@@ -70,6 +71,8 @@ class AddressBookServiceTest {
             assertPerson(response, juan, i, HttpStatus.CREATED)
             // Check that the new user exists
             response = restTemplate.getForEntity(juanURI, Person::class.java)
+            // [response] change every same POST request -> not idempotent
+            // POST request change [personList] resource at the server everytime -> not safe
             assertPerson(response, juan, i, HttpStatus.OK)
             assertEquals(i, addressBook.personList.size)
         }
@@ -88,14 +91,17 @@ class AddressBookServiceTest {
         val salvador = Person(id = addressBook.nextId(), name = "Salvador")
         addressBook.personList.add(salvador)
 
+        // Check [people] was introduced correctly
+        assertEquals(1, addressBook.personList.size)
+
         // Prepare data
-        val people = List(4) {
-            Person(name = "Juan");
-            Person(name = "Pedro");
-            Person(name = "Maria");
+        val people = listOf(
+            Person(name = "Juan"),
+            Person(name = "Pedro"),
+            Person(name = "Maria"),
             Person(name = "Carmen")
-        }
-        // Loop
+        )
+        // Tests users creation
         people.forEachIndexed { i, person ->
             val personURI = URI.create(
                 "http://localhost:$port/contacts/person/${i + 2}"
@@ -113,6 +119,8 @@ class AddressBookServiceTest {
             // Check that the new user exists
             (1..3).forEach { _ ->
                 response = restTemplate.getForEntity(personURI, Person::class.java)
+                // [response] change every same POST request -> not idempotent
+                // POST request change [personList] resource at the server everytime -> not safe
                 assertPerson(response, person, i + 2, HttpStatus.OK)
                 assertEquals(i + 2, addressBook.personList.size)
             }
@@ -131,24 +139,42 @@ class AddressBookServiceTest {
     fun listUsers() {
 
         // Prepare server
-        val people = List(4) {
-            Person(name = "Juan", id = addressBook.nextId());
-            Person(name = "Pedro", id = addressBook.nextId());
-            Person(name = "Maria", id = addressBook.nextId());
+        val people = listOf(
+            Person(name = "Juan", id = addressBook.nextId()),
+            Person(name = "Pedro", id = addressBook.nextId()),
+            Person(name = "Maria", id = addressBook.nextId()),
             Person(name = "Carmen", id = addressBook.nextId())
-        }
+        )
         people.forEach { person ->
             addressBook.personList.add(person)
         }
 
+        // Check [people] was introduced correctly
+        assertEquals(people.size, addressBook.personList.size)
+
         // Test list of contacts
-        val response =
-            restTemplate.getForEntity("http://localhost:$port/contacts", Array<Person>::class.java)
-        people.forEachIndexed { i, person ->
-            assertEquals(200, response.statusCode.value())
-            assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-            assertEquals(people.size, response.body?.size)
-            assertEquals(person.name, response.body?.get(i)?.name)
+        val n = (1..3)
+        n.forEach { _ ->
+            val response =
+                restTemplate.getForEntity(
+                    "http://localhost:$port/contacts",
+                    Array<Person>::class.java
+                )
+            people.forEachIndexed { i, person ->
+                val personURI = null
+                // Check headers
+                assertEquals(HttpStatus.OK, response.statusCode)
+                assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
+                // [response] doesn't change every same GET request -> idempotent
+                // GET request doesn't change [personList] resource at the server everytime -> not safe
+                // Check [response]
+                assertEquals(people.size, response.body?.size)
+                assertPersonInBody(person, person.id, response.body?.get(i), personURI)
+                // Check [personList]
+                assertPersonInPersonList(person, person.id, personURI)
+            }
+            // Check if [personList] is [people] size
+            assertEquals(people.size, addressBook.personList.size)
         }
 
         //////////////////////////////////////////////////////////////////////
@@ -160,35 +186,54 @@ class AddressBookServiceTest {
     @Test
     fun updateUsers() {
         // Prepare server
-        val salvador = Person(name = "Salvador", id = addressBook.nextId())
-        val juan = Person(name = "Juan", id = addressBook.nextId())
-        val juanURI = URI.create("http://localhost:$port/contacts/person/2")
-        addressBook.personList.add(salvador)
-        addressBook.personList.add(juan)
+        val people = listOf(
+            Person(name = "Salvador", id = addressBook.nextId()),
+            Person(name = "Juan", id = addressBook.nextId()),
+        )
+
+        people.forEach { person ->
+            addressBook.personList.add(person)
+        }
+
+        // Check [people] was introduced correctly
+        assertEquals(people.size, addressBook.personList.size)
+
+        // Get [juan] info
+        val juan = people.last()
+        val juanURI = URI.create("http://localhost:$port/contacts/person/${juan.id}")
 
         // Update Maria
         val maria = Person(name = "Maria")
 
-        var response =
-            restTemplate.exchange(juanURI, HttpMethod.PUT, HttpEntity(maria), Person::class.java)
-        assertEquals(204, response.statusCode.value())
+        val n = (1..3)
+        n.forEach { _ ->
+            var response =
+                restTemplate.exchange(
+                    juanURI,
+                    HttpMethod.PUT,
+                    HttpEntity(maria),
+                    Person::class.java
+                )
+            assertEquals(HttpStatus.NO_CONTENT, response.statusCode)
+            // Verify that the update is real
+            response = restTemplate.getForEntity(juanURI, Person::class.java)
+            // [response] doesn't change every same PUT request -> idempotent
+            // PUT request change [personList] resource at the server everytime -> not safe
+            val pretendUpdatedMaria = Person(name = maria.name, id = juan.id, href = juanURI)
+            assertPerson(response, pretendUpdatedMaria, juan.id, HttpStatus.OK)
 
-        // Verify that the update is real
-        response = restTemplate.getForEntity(juanURI, Person::class.java)
-        assertEquals(200, response.statusCode.value())
-        assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-        val updatedMaria = response.body
-        assertEquals(maria.name, updatedMaria?.name)
-        assertEquals(2, updatedMaria?.id)
-        assertEquals(juanURI, updatedMaria?.href)
+        }
 
         // Verify that only can be updated existing values
-        restTemplate.execute("http://localhost:$port/contacts/person/3", HttpMethod.PUT,
+        restTemplate.execute("http://localhost:$port/contacts/person/${people.size + 1}",
+            HttpMethod.PUT,
             {
                 it.headers.contentType = MediaType.APPLICATION_JSON
-                ObjectMapper().writeValue(it.body, maria)
+                people.forEach { person ->
+                    ObjectMapper().writeValue(it.body, person)
+                }
             },
-            { assertEquals(404, it.statusCode.value()) }
+            { assertEquals(HttpStatus.NOT_FOUND, it.statusCode) }
         )
 
         //////////////////////////////////////////////////////////////////////
@@ -200,25 +245,43 @@ class AddressBookServiceTest {
     @Test
     fun deleteUsers() {
         // Prepare server
-        val salvador = Person(name = "Salvador", id = addressBook.nextId())
-        val juan = Person(name = "Juan", id = addressBook.nextId())
-        val juanURI = URI.create("http://localhost:$port/contacts/person/2")
-        addressBook.personList.add(salvador)
-        addressBook.personList.add(juan)
+        val people = listOf(
+            Person(name = "Salvador", id = addressBook.nextId()),
+            Person(name = "Juan", id = addressBook.nextId()),
+        )
 
-        // Delete a user
-        restTemplate.execute(
-            juanURI,
-            HttpMethod.DELETE,
-            {},
-            { assertEquals(204, it.statusCode.value()) })
+        people.forEach { person ->
+            addressBook.personList.add(person)
+        }
+        // Check [people] was introduced correctly
+        assertEquals(people.size, addressBook.personList.size)
 
-        // Verify that the user has been deleted
-        restTemplate.execute(
-            juanURI,
-            HttpMethod.GET,
-            {},
-            { assertEquals(404, it.statusCode.value()) })
+        // Tests users deletion
+        people.forEach { person ->
+            val personUri = URI.create("http://localhost:$port/contacts/person/${person.id}")
+            // Delete a user
+            restTemplate.execute(
+                personUri,
+                HttpMethod.DELETE,
+                {},
+                { assertEquals(HttpStatus.NO_CONTENT, it.statusCode) })
+            (1..3).forEach { _ ->
+                // Verify that the user has been deleted
+                restTemplate.execute(
+                    personUri,
+                    HttpMethod.GET,
+                    {},
+                    { assertEquals(HttpStatus.NOT_FOUND, it.statusCode) })
+                // [response] change every same DELETE request -> idempotent
+                // DELETE request change [personList] resource at the server everytime -> not safe
+                // Check if it is not in [personList]
+                val personFoundList = addressBook.personList.filter {
+                    it.name == person.name && it.id == person.id
+                }
+                assertEquals(0, personFoundList.size)
+            }
+        }
+        assertEquals(0, addressBook.personList.size)
 
         //////////////////////////////////////////////////////////////////////
         // Verify that DELETE /contacts/person/2 is well implemented by the service, i.e
@@ -229,41 +292,48 @@ class AddressBookServiceTest {
     @Test
     fun findUsers() {
         // Prepare server
-        val salvador = Person(name = "Salvador", id = addressBook.nextId())
-        val juan = Person(name = "Juan", id = addressBook.nextId())
-        val salvadorURI = URI.create("http://localhost:$port/contacts/person/1")
-        val juanURI = URI.create("http://localhost:$port/contacts/person/2")
-        addressBook.personList.add(salvador)
-        addressBook.personList.add(juan)
+        val people = listOf(
+            Person(name = "Salvador", id = addressBook.nextId()),
+            Person(name = "Juan", id = addressBook.nextId()),
+        )
 
-        // Test user 1 exists
-        var response = restTemplate.getForEntity(salvadorURI, Person::class.java)
-        assertEquals(200, response.statusCode.value())
-        assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-        var person = response.body
-        assertEquals(salvador.name, person?.name)
-        assertEquals(salvador.id, person?.id)
-        assertEquals(salvador.href, person?.href)
+        val peoplesUri: MutableList<URI> = mutableListOf()
 
-        // Test user 2 exists
-        response = restTemplate.getForEntity(juanURI, Person::class.java)
-        assertEquals(200, response.statusCode.value())
-        assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-        person = response.body
-        assertEquals(juan.name, person?.name)
-        assertEquals(juan.id, person?.id)
-        assertEquals(juan.href, person?.href)
+        people.forEach { person ->
+            addressBook.personList.add(person)
+            peoplesUri.add(URI.create("http://localhost:$port/contacts/person/${person.id}"))
+        }
 
-        // Test user 3 doesn't exist
+        // Check [people] was introduced correctly
+        assertEquals(people.size, addressBook.personList.size)
+
+        // Test users in [people] exist
+        (people.indices).forEach { i ->
+            val person = people[i]
+            val personURI = null
+            val response = restTemplate.getForEntity(peoplesUri[i], Person::class.java)
+            // Check [response] body
+            assertPersonInBody(person, person.id, response.body, personURI)
+            // Check [personList]
+            assertPersonInPersonList(person, person.id, personURI)
+        }
+        // Check [personList] is empty
+        assertEquals(people.size, addressBook.personList.size)
+
+        // Test last user doesn't exist
         restTemplate.execute(
-            "http://localhost:$port/contacts/person/3",
+            "http://localhost:$port/contacts/person/${people.size + 1}",
             HttpMethod.GET,
             {},
-            { assertEquals(404, it.statusCode.value()) })
+            { assertEquals(HttpStatus.NOT_FOUND, it.statusCode) })
     }
 
     /* ---- Private methods ---- */
 
+    /**
+     * Checks if [person] is found whether in [response] body or in the personList server database.
+     * also checks the [response] headers
+     */
     private fun assertPerson(
         response: ResponseEntity<Person>,
         person: Person,
@@ -271,9 +341,30 @@ class AddressBookServiceTest {
         httpStatus: HttpStatus
     ) {
         val uri = URI.create("http://localhost:$port/contacts/person/$id")
+        // Check response headers
         assertEquals(httpStatus, response.statusCode)
         assertEquals(MediaType.APPLICATION_JSON, response.headers.contentType)
-        val personUpdated = response.body
+
+        assertPersonInPersonList(person, id, uri)
+        assertPersonInBody(person, id, response.body, uri)
+    }
+
+    /**
+     * Checks if [person] is found in personList server database
+     */
+    private fun assertPersonInPersonList(person: Person, id: Int, uri: URI?) {
+        // Find in [personList] at the server database
+        val personListUpdated = addressBook.personList[id - 1]
+        assertEquals(person.name, personListUpdated.name)
+        assertEquals(id, personListUpdated.id)
+        assertEquals(uri, personListUpdated.href)
+    }
+
+    /**
+     * Checks if [person] is equal to changed [personUpdated] in the response body
+     */
+    private fun assertPersonInBody(person: Person, id: Int, personUpdated: Person?, uri: URI?) {
+        // Check response body
         assertEquals(person.name, personUpdated?.name)
         assertEquals(id, personUpdated?.id)
         assertEquals(uri, personUpdated?.href)
